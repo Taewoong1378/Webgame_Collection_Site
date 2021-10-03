46b5a882eedff672b553053df042039dd845768d
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-10-03 11:24:32 +0900

    navbar 기본 구성 완료

--- front/components/Navbar.js
+++ front/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useMemo, useRef, useState } from 'react';
 import { Menu } from 'antd';
-import { SettingOutlined } from '@ant-design/icons';
+import { CrownOutlined, FireOutlined, LikeOutlined, ThunderboltOutlined, TrophyOutlined } from '@ant-design/icons';
 
 const { SubMenu } = Menu;
 
@@ -15,34 +15,32 @@ const NavBar = () => {
   return (
     <div style={style}>
         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-        <SubMenu key="SubMenu1" icon={<SettingOutlined />} title="반응속도체크 게임">
-            <Menu.ItemGroup title="Item 1">
+        <SubMenu key="SubMenu1" icon={<ThunderboltOutlined />} title="반응속도체크 게임">
+            <Menu.ItemGroup title="반응속도체크">
             <Menu.Item key="setting:1">게임하기</Menu.Item>
             <Menu.Item key="setting:2">점수 확인</Menu.Item>
             </Menu.ItemGroup>
         </SubMenu>
-        <SubMenu key="SubMenu2" icon={<SettingOutlined />} title="가위바위보 게임">
-            <Menu.ItemGroup title="Item 2">
+        <SubMenu key="SubMenu2" icon={<LikeOutlined />} title="가위바위보 게임">
+            <Menu.ItemGroup title="가위바위보">
             <Menu.Item key="setting:3">게임하기</Menu.Item>
             <Menu.Item key="setting:4">점수 확인</Menu.Item>
             </Menu.ItemGroup>
         </SubMenu>
-        <SubMenu key="SubMenu3" icon={<SettingOutlined />} title="숫자야구 게임">
-            <Menu.ItemGroup title="Item 3">
+        <SubMenu key="SubMenu3" icon={<TrophyOutlined />} title="숫자야구 게임">
+            <Menu.ItemGroup title="숫자야구">
             <Menu.Item key="setting:5">게임하기</Menu.Item>
             <Menu.Item key="setting:6">점수 확인</Menu.Item>
             </Menu.ItemGroup>
         </SubMenu>
-        <SubMenu key="SubMenu4" icon={<SettingOutlined />} title="지뢰찾기 게임">
-            <Menu.ItemGroup title="Item 3">
+        <SubMenu key="SubMenu4" icon={<FireOutlined />} title="지뢰찾기 게임">
+            <Menu.ItemGroup title="지뢰찾기">
             <Menu.Item key="setting:5">게임하기</Menu.Item>
             <Menu.Item key="setting:6">점수 확인</Menu.Item>
             </Menu.ItemGroup>
         </SubMenu>
-        <Menu.Item key="alipay">
-            <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            네 번째 메뉴 - 링크
-            </a>
+        <Menu.Item key="alipay" icon={<CrownOutlined />}>
+            <a href="none">최종 순위</a>
         </Menu.Item>
         </Menu>
     </div>
